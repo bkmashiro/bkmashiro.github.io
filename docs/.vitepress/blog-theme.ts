f1c91c165f690060229a2c5eab5c97e3f191b73a
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2024-05-19 10:55:44 +0800

    add 0002-0062

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -5,16 +5,6 @@ import { getThemeConfig } from '@sugarat/theme/node'
 // import type { Theme } from '@sugarat/theme'
 
 const baseUrl = 'https://blog.yuzhes.com'
-// const RSS: Theme.RSSOptions = {
-//   title: '粥里有勺糖',
-//   baseUrl,
-//   copyright: 'Copyright (c) 2018-present, 粥里有勺糖',
-//   description: '你的指尖,拥有改变世界的力量（大前端相关技术分享）',
-//   language: 'zh-cn',
-//   image: 'https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030',
-//   favicon: 'https://sugarat.top/favicon.ico',
-// }
-
 // 所有配置项，详见文档: https://theme.sugarat.top/
 const blogTheme = getThemeConfig({
   // 开启RSS支持
@@ -27,17 +17,7 @@ const blogTheme = getThemeConfig({
 
   // 页脚
   footer: {
-    // message 字段支持配置为HTML内容，配置多条可以配置为数组
-    // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
     copyright: 'baka_mashiro',
-    // icpRecord: {
-    //   name: '蜀ICP备19011724号',
-    //   link: 'https://beian.miit.gov.cn/'
-    // },
-    // securityRecord: {
-    //   name: '公网安备xxxxx',
-    //   link: 'https://www.beian.gov.cn/portal/index.do'
-    // },
     version: false,
   },
 
--- docs/posts/typescript/medium/template.md
+++ docs/posts/typescript/medium/template.md
@@ -1,27 +1,20 @@
 ---
-description: TypeChallenge - 5117
-title: 去除数组指定元素
+description: TypeChallenge - TODO
+title: TODO
 readingTime: true
 tag:
   - TypeScript
   - TypeChallenge
+  - TC-Medium
 outline: [2, 3]
 hidden: true
-publish: false
 ---
+<!-- THIS IS A TEMPLATE -->
 
-# 去除数组指定元素
-
+# TODO
+[题目链接](https://tsch.js.org/TODO)
 ## 题目
 
-实现一个像 Lodash.without 函数一样的泛型 Without<T, U>，它接收数组类型的 T 和数字或数组类型的 U 为参数，会返回一个去除 U 中元素的数组 T。
-
-例如：
-
-```ts
-type Res = Without<[1, 2], 1>; // expected to be [2]
-type Res1 = Without<[1, 2, 4, 1, 5], [1, 2]>; // expected to be [4, 5]
-type Res2 = Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>; // expected to be []
-```
+TODO
 
 ## 解答
