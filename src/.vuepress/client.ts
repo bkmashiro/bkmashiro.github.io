f7729a86d2bb9406cb7baa322ca7a1e4018d3f4b
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2024-08-18 18:55:11 +0800

    add analytics

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,6 @@
+import { defineClientConfig } from "vuepress/client";
+import Analytics from "./components/Analytics.vue";
+
+export default defineClientConfig({
+  rootComponents: [Analytics],
+});
---
+++ src/.vuepress/components/Analytics.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SpeedInsights />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { inject } from "@vercel/analytics"
+import { onMounted } from "vue/dist/vue.mjs";
+
+onMounted(() => {
+  inject()
+})
+
+</script>
+
+<style scoped>
+
+</style>
